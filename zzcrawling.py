a767b93957aebf49fc7f503fd90eb41f119efd41
9gi_coach21 <9gi_coach21@ssafy.com>
2024-03-19 17:39:43 +0900

    feat : 크롤링 - 치지직 링크 수정

--- zzcrawling.py
+++ zzcrawling.py
@@ -36,114 +36,104 @@ try:
     # 페이지 스크롤 다운을 위한 루프
     scroll_position = 0
     last_height = 0
-    # while True:
-    #
-    #     new_height = driver.execute_script("return document.body.scrollHeight")
-    #
-    #     # 페이지의 끝까지 스크롤 다운
-    #     # driver.execute_script("window.scrollTo(0, document.body.scrollHeight/3 * 2);")
-    #
-    #     # 페이지를 조금씩 내리는 스크롤 (예: 100픽셀씩)
-    #     # scroll_position += (new_height - last_height) * 2 / 5
-    #     scroll_position += 300
-    #     driver.execute_script(f"window.scrollTo(0, {scroll_position});")
-    #
-    #     # 새로운 페이지 콘텐츠 로드를 기다림
-    #     time.sleep(SCROLL_PAUSE_TIME)
-    #
-    #     # 새로운 높이를 계산하고, 이전 높이와 비교
-    #     # new_height = driver.execute_script("return document.body.scrollHeight")
-    #     html = driver.page_source
-    #     soup = BeautifulSoup(html, 'html.parser')
-    #
-    #     view_cnt = soup.find_all('span', {'class', 'video_card_badge__w02UD'})
-    #     # view_cnt = driver.find_elements('video_card_badge__w02UD')
-    #     # print(len(view_cnt))
-    #     # print(view_cnt[len(view_cnt)-1].text.strip())
-    #     cnt = re.sub(r'\D', '', view_cnt[len(view_cnt)-1].text.strip())
-    #     # print(cnt)
-    #     if int(cnt) < 20:
-    #         break
-    #     scroll_position = new_height
-        # last_height = new_height
+    while True:
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        # 페이지를 조금씩 내리는 스크롤 (예: 100픽셀씩)
+        scroll_position = last_height + (new_height - last_height) * 1 / 5
+        # scroll_position += 300
+        driver.execute_script(f"window.scrollTo(0, {scroll_position});")
+
+        # 새로운 페이지 콘텐츠 로드를 기다림
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        view_cnt = driver.find_elements(By.XPATH, "//*[@id='layout-body']/div/section/ul/li")
+
+        last_view = view_cnt[len(view_cnt) - 1]
+
+        last_view_cnt = last_view.find_element(By.CLASS_NAME, "video_card_badge__w02UD")
+
+        cnt = re.sub(r'\D', '', last_view_cnt.text.strip())
+
+        if int(cnt) < 20:
+            break
+
+        last_height = new_height
 
 
-
-    # 'component_container__CTlNd' 클래스를 가진 section 요소 찾기
-    component_container = soup.find_all('section', {'class','component_section__lGX4U'})
-    print(component_container[0])
     # 시청자 수를 저장할 리스트 초기화
-    # streamer_list = []
-    #
-    # # 각 파트너 항목에서 시청자 수 추출
-    # streamer_items = component_container[0].find_all('li', {'class','component_item__WsLOa'})
-    # index = 0
-    # for item in streamer_items:
-    #     # 'video_card_badge__w02UD' 클래스를 가진 요소의 텍스트 추출 - 시청자 수
-    #     viewer_count = item.find('span', {'class', 'video_card_badge__w02UD'})
-    #     if viewer_count:
-    #         count = re.sub(r'\D', '', viewer_count.text.strip())
-    #         if int(count) < 20:
-    #             break
-    #     data = []
-    #     index += 1
-    #     data.append(index)
-    #
-    #     # 'name_text__yQG50' 클래스를 가진 요소의 텍스트 추출 - 방송인 이름
-    #     streamer_name = item.find('span', {'class', 'name_text__yQG50'})
-    #     if streamer_name:
-    #         data.append(streamer_name.text.strip())
-    #
-    #     click_streamer = item.find('a', {'class', 'video_card_image__yHXqv'})
-    #
-    #     # print(click_streamer.get('href'))
-    #
-    #     # <a> 태그의 href 속성 값을 가져옴
-    #     href_value = click_streamer.get('href')
-    #
-    #     # print(href_value)
-    #
-    #     # 자바스크립트를 사용하여 새 탭에서 href URL 열기
-    #     driver.execute_script(f"window.open('https://chzzk.naver.com' + '{href_value}');")
-    #
-    #     # 새 탭으로 스위치
-    #     driver.switch_to.window(driver.window_handles[1])
-    #
-    #     channel_profile = driver.find_element(By.CLASS_NAME, 'channel_profile_cell__kkiQb')
-    #
-    #     data.append(channel_profile.text)
-    #
-    #     driver.close()  # 새 탭 닫기
-    #     driver.switch_to.window(driver.window_handles[0])  # 원래 탭으로 스위치
-    #
-    #     # 'video_card_title__Amjk2' 클래스를 가진 요소의 텍스트 추출 - 제목
-    #     live_title = item.find('a', {'class', 'video_card_title__Amjk2'})
-    #     if live_title:
-    #         blind_text = live_title.find('span', {'class','blind'}).get_text()
-    #         data.append(live_title.text.replace(blind_text, '').strip())
-    #
-    #     # 시청자 수 데이터
-    #     data.append(viewer_count.text.strip())
-    #
-    #     # 'video_card_category__xQ15T' 클래스를 가진 요소의 텍스트 추출 - 태그
-    #     live_tag = item.find('span', {'class', 'video_card_category__xQ15T'})
-    #     if live_tag:
-    #         data.append(live_tag.text.strip())
-    #     else:
-    #         data.append("없음")
-    #
-    #     # 'video_card_image__yHXqv' 클래스를 가진 요소의 텍스트 추출 - 썸네일
-    #     live_img = item.find('img', {'class', 'video_card_image__yHXqv'})
-    #     if live_img:
-    #         data.append(live_img.get('src'))
-    #
-    #
-    #     streamer_list.append(data)
-    #
-    #
-    #
-    # # 결과 출력
-    # print(tabulate(streamer_list, headers=["번호", "방송인", "팔로우", "제목", "시청자 수", "태그", "썸네일"]))
+    streamer_list = []
+
+    # 각 파트너 항목에서 시청자 수 추출
+    streamer_items = driver.find_elements(By.XPATH, "//*[@id='layout-body']/div/section/ul/li")
+    index = 0
+    for item in streamer_items:
+        # 'video_card_badge__w02UD' 클래스를 가진 요소의 텍스트 추출 - 시청자 수
+        viewer_count = item.find_element(By.CLASS_NAME, "video_card_badge__w02UD")
+        if viewer_count:
+            count = re.sub(r'\D', '', viewer_count.text.strip())
+            if int(count) < 20:
+                break
+        data = []
+        index += 1
+        data.append(index)
+
+        # 'name_text__yQG50' 클래스를 가진 요소의 텍스트 추출 - 방송인 이름
+        streamer_name = item.find('span', {'class', 'name_text__yQG50'})
+        if streamer_name:
+            data.append(streamer_name.text.strip())
+
+        click_streamer = item.find('a', {'class', 'video_card_image__yHXqv'})
+
+        # print(click_streamer.get('href'))
+
+        # <a> 태그의 href 속성 값을 가져옴
+        href_value = click_streamer.get('href')
+
+        # print(href_value)
+
+        # 자바스크립트를 사용하여 새 탭에서 href URL 열기
+        driver.execute_script(f"window.open('https://chzzk.naver.com' + '{href_value}');")
+
+        # 새 탭으로 스위치
+        driver.switch_to.window(driver.window_handles[1])
+
+        channel_profile = driver.find_element(By.CLASS_NAME, 'channel_profile_cell__kkiQb')
+
+        data.append(channel_profile.text)
+
+        driver.close()  # 새 탭 닫기
+        driver.switch_to.window(driver.window_handles[0])  # 원래 탭으로 스위치
+
+        # 'video_card_title__Amjk2' 클래스를 가진 요소의 텍스트 추출 - 제목
+        live_title = item.find('a', {'class', 'video_card_title__Amjk2'})
+        if live_title:
+            blind_text = live_title.find('span', {'class','blind'}).get_text()
+            data.append(live_title.text.replace(blind_text, '').strip())
+
+        # 시청자 수 데이터
+        data.append(viewer_count.text.strip())
+
+        # 'video_card_category__xQ15T' 클래스를 가진 요소의 텍스트 추출 - 태그
+        live_tag = item.find('span', {'class', 'video_card_category__xQ15T'})
+        if live_tag:
+            data.append(live_tag.text.strip())
+        else:
+            data.append("없음")
+
+        # 'video_card_image__yHXqv' 클래스를 가진 요소의 텍스트 추출 - 썸네일
+        live_img = item.find('img', {'class', 'video_card_image__yHXqv'})
+        if live_img:
+            data.append(live_img.get('src'))
+
+
+        streamer_list.append(data)
+
+
+
+    # 결과 출력
+    print(tabulate(streamer_list, headers=["번호", "방송인", "팔로우", "제목", "시청자 수", "태그", "썸네일"]))
 
     # 브라우저 종료
     driver.quit()
